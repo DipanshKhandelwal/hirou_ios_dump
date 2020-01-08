8834a013f9f00e59e46ac0c3c1469ab7143b29ce
DipanshKhandelwal <deepanshrocks438@gmail.com>
2020-01-08 21:20:53 +0530

    Initialized app in master detail view

---
+++ hirouTests/hirouTests.swift
@@ -0,0 +1,34 @@
+//
+//  hirouTests.swift
+//  hirouTests
+//
+//  Created by Dipansh Khandelwal on 08/01/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import XCTest
+@testable import hirou
+
+class hirouTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
