5424e50a1d12d17acb3500644aaab46fe401be25
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-07 01:33:49 +0530

    Add a draggable pointer in CollectionPointFormViewController

--- hirou/CreateBaseRoute/CollectionPoint/CollectionPointFormViewController.swift
+++ hirou/CreateBaseRoute/CollectionPoint/CollectionPointFormViewController.swift
@@ -22,6 +22,8 @@ class CollectionPointFormViewController: UIViewController, MGLMapViewDelegate {
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     
+    var annotationView: MGLAnnotationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cpMapView.delegate = self
@@ -170,7 +172,6 @@ class CollectionPointFormViewController: UIViewController, MGLMapViewDelegate {
         }
     }
     
-    
     func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
         return true
     }
@@ -182,5 +183,104 @@ class CollectionPointFormViewController: UIViewController, MGLMapViewDelegate {
         label.text = "CP"
         
         return label
+    }
+    
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+        // This example is only concerned with point annotations.
+        guard annotation is MGLPointAnnotation else {
+            return nil
+        }
+//
+        // For better performance, always try to reuse existing annotations. To use multiple different annotation views, change the reuse identifier for each.
+        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "draggablePoint") {
+            return annotationView
+        } else {
+//            return DraggableAnnotationView(reuseIdentifier: "draggablePoint", size: 20)
+//            return MGLAnnotationView(annotation: annotation, reuseIdentifier: "draggablePoint" )
+            self.annotationView = DraggableAnnotationView(annotation: annotation as! MGLPointAnnotation, reuseIdentifier: "draggablePoint", size: 20)
+            return self.annotationView
+        }
+    }
+}
+
+class DraggableAnnotationView: MGLAnnotationView {
+    init(annotation: MGLPointAnnotation, reuseIdentifier: String, size: CGFloat) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        
+        // `isDraggable` is a property of MGLAnnotationView, disabled by default.
+        isDraggable = true
+        
+        // This property prevents the annotation from changing size when the map is tilted.
+        scalesWithViewingDistance = false
+        
+        // Begin setting up the view.
+        frame = CGRect(x: 0, y: 0, width: size, height: size)
+        
+        backgroundColor = .red
+        
+        // Use CALayer’s corner radius to turn this view into a circle.
+        layer.cornerRadius = size / 2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.1
+    }
+    
+    // These two initializers are forced upon us by Swift.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Custom handler for changes in the annotation’s drag state.
+    override func setDragState(_ dragState: MGLAnnotationViewDragState, animated: Bool) {
+        super.setDragState(dragState, animated: animated)
+        
+        switch dragState {
+        case .starting:
+//            print("Starting", annotation?.coordinate.longitude)
+            startDragging()
+        case .dragging:
+//            print(".", terminator: "")
+            print("")
+        case .ending, .canceling:
+//            print("Ending", annotation?.coordinate.latitude)
+            endDragging()
+        case .none:
+            break
+        @unknown default:
+            fatalError("Unknown drag state")
+        }
+    }
+    
+    // When the user interacts with an annotation, animate opacity and scale changes.
+    func startDragging() {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.layer.opacity = 0.8
+            self.transform = CGAffineTransform.identity.scaledBy(x: 1.5, y: 1.5)
+        }, completion: nil)
+        
+        // Initialize haptic feedback generator and give the user a light thud.
+        if #available(iOS 10.0, *) {
+            let hapticFeedback = UIImpactFeedbackGenerator(style: .light)
+            hapticFeedback.impactOccurred()
+        }
+    }
+    
+    func endDragging() {
+        transform = CGAffineTransform.identity.scaledBy(x: 1.5, y: 1.5)
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.layer.opacity = 1
+            self.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
+        }, completion: nil)
+        
+        // Give the user more haptic feedback when they drop the annotation.
+        if #available(iOS 10.0, *) {
+            let hapticFeedback = UIImpactFeedbackGenerator(style: .light)
+            hapticFeedback.impactOccurred()
+        }
     }
 }
