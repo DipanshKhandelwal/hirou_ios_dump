21e8130ff326e62427b352178b54262feba598f2
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-12 05:12:43 +0530

    Add custom PointAnnotation as CollectionPointPointAnnotation

---
+++ hirou/Components/CollectionPointPointAnnotation.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionPointPointAnnotation.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 12/04/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+import Mapbox
+
+class CollectionPointPointAnnotation: MGLPointAnnotation {
+    var collectionPoint: CollectionPoint!
+    
+    init(collectionPoint: CollectionPoint) {
+        super.init()
+        self.collectionPoint = collectionPoint
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
