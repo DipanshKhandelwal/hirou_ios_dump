ad4b05e4b9ad5e6bd976d3889e67711243410e33
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-16 23:44:26 +0530

    Add TaskDetailViewController

---
+++ hirou/Management/TaskDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskDetailViewController.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 16/04/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
