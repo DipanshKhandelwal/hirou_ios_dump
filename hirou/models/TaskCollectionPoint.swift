b9ee2ebf6c2a82174deb40ebdd5750a2328f1c16
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-21 17:39:06 +0530

    Add static method to parse TaskCollectionPoint object from json response

--- hirou/models/TaskCollectionPoint.swift
+++ hirou/models/TaskCollectionPoint.swift
@@ -13,19 +13,43 @@ class TaskCollectionPoint {
     var id: Int
     var name: String
     var address: String
-    var taskRoute: Int
     var location: Location
     var sequence: Int
     var image: String
+    var taskCollections: [TaskCollection]
 
-    init?(id: Int, name : String, address: String, taskRoute: Int, location: Location, sequence: Int, image: String) {
+    init?(id: Int, name : String, address: String, location: Location, sequence: Int, image: String, taskCollections: [TaskCollection]) {
         // Initialize stored properties.
         self.id = id
         self.name = name
         self.address = address
-        self.taskRoute = taskRoute
         self.location = location
         self.sequence = sequence
         self.image = image
+        self.taskCollections = taskCollections
+    }
+    
+    static func getTaskCollectionPointFromResponse(obj : AnyObject) -> TaskCollectionPoint {
+        let name = obj["name"] as! String
+        let id = obj["id"] as! Int
+        let address = obj["address"] as! String
+        
+        let locationCoordinates = (obj["location"] as! String).split{$0 == ","}.map(String.init)
+        let location = Location( latitude: locationCoordinates[0], longitude : locationCoordinates[1] )
+
+        let sequence = obj["sequence"] as! Int
+        
+        let image = ""
+        
+        var taskCollections = [TaskCollection]()
+        let taskCollectionsResponse = obj["task_collection"] as AnyObject
+        for tc in taskCollectionsResponse as! [Any] {
+            let taskCollectionResponse = (tc as AnyObject)
+            let taskCollection = TaskCollection.getTaskCollectionFromResponse(obj: taskCollectionResponse)
+            taskCollections.append(taskCollection)
+        }
+
+        let taskCollectionPointObj = TaskCollectionPoint(id: id, name: name, address: address, location: location, sequence: sequence, image: image, taskCollections: taskCollections);
+        return taskCollectionPointObj!
     }
 }
