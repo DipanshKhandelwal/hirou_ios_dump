7eb196f669f4e4b7dab172011068ee8c3d25a7bd
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-21 17:42:53 +0530

    Add static method to parse TaskRoute object from json response

--- hirou/models/TaskRoute.swift
+++ hirou/models/TaskRoute.swift
@@ -14,12 +14,35 @@ class TaskRoute {
     var name: String
     var customer: Customer
     var date: String
+    var taskCollectionPoints: [TaskCollectionPoint]
     
-    init?(id: Int, name : String, customer: Customer, date: String) {
+    init?(id: Int, name : String, customer: Customer, date: String, taskCollectionPoints: [TaskCollectionPoint]) {
         // Initialize stored properties.
         self.id = id
         self.name = name
         self.customer = customer
         self.date = date
+        self.taskCollectionPoints = taskCollectionPoints
+    }
+    
+    static func getTaskRouteFromResponse(obj: AnyObject) -> TaskRoute{
+        let id = obj["id"] as! Int
+        let name = obj["name"] as! String
+        
+        let customerResponse = obj["customer"] as AnyObject
+        let customer = Customer.getCustomerFromResponse(obj: customerResponse)
+        
+        let date = obj["date"] as! String
+        
+        var taskCollectionPoints = [TaskCollectionPoint]()
+        let taskCollectionPointsResponse = obj["task_collection_point"] as AnyObject
+        for tc in taskCollectionPointsResponse as! [Any] {
+            let taskCollectionPointResponse = (tc as AnyObject)
+            let taskCollectionPoint = TaskCollectionPoint.getTaskCollectionPointFromResponse(obj: taskCollectionPointResponse)
+            taskCollectionPoints.append(taskCollectionPoint)
+        }
+
+        let taskRouteObj = TaskRoute(id: id, name: name, customer: customer, date: date, taskCollectionPoints: taskCollectionPoints)
+        return taskRouteObj!
     }
 }
