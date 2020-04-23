aa25510afdafb4db7cf519e3e7a213038fe4b5b4
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-04-23 12:56:59 +0530

    Fix Vehicle class & add static function to get Vehicle object from json

--- hirou/models/Vehicle.swift
+++ hirou/models/Vehicle.swift
@@ -10,21 +10,32 @@ import Foundation
 
 class Vehicle {
     //MARK: Properties
-    var registrationNumber: String = ""
-    var model : String = ""
-    var location : Location?
-    var users: [String] = []
+    var id: Int
+    var registrationNumber: String
+    var model : String
+    var location : Location
     
-    init?(registrationNumber : String, model: String, location: Location, users: [String]) {
+    init?(id: Int, registrationNumber : String, model: String, location: Location) {
         // Initialization should fail if there is no registration number.
         if registrationNumber.isEmpty  {
             return nil
         }
         
         // Initialize stored properties.
+        self.id = id
         self.registrationNumber = registrationNumber
         self.model = model
         self.location = location
-        self.users = users
+    }
+
+    static func getVehicleFromResponse(obj : AnyObject) -> Vehicle {
+        let id = obj["id"] as! Int
+        let registrationNumber = obj["registration_number"] as! String
+        let model = obj["model"] as! String
+        
+        let locationCoordinates = (obj["location"] as! String).split{$0 == ","}.map(String.init)
+        let location = Location( latitude: locationCoordinates[0], longitude : locationCoordinates[1] )
+        let vehicleObj = Vehicle(id: id, registrationNumber: registrationNumber, model: model, location: location)
+        return vehicleObj!
     }
 }
