7ae55f6faa52e1c26768ac66a1cef76ca18fe286
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-07 10:02:58 +0530

    Fix name of CollectionsTableViewController

--- hirou/Management/TaskCollection/CollectionsTableViewController.swift
+++ hirou/Management/TaskCollection/CollectionsTableViewController.swift
@@ -12,12 +12,13 @@ class CollectionsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.title = "Collections"
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
