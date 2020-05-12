8a7b7bc398712ca816b1e28ac108957036c69b65
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-12 18:02:02 +0530

    Fix CollectionPoint data class
    Fix Decodable property

--- hirou/models/CollectionPoint.swift
+++ hirou/models/CollectionPoint.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class CollectionPoint: Encodable, Decodable {
+struct CollectionPoint: Encodable, Decodable {
     //MARK: Properties
     var id: Int
     var name: String
@@ -16,7 +16,7 @@ class CollectionPoint: Encodable, Decodable {
     var route: Int
     var location: Location
     var sequence: Int
-    var image: String
+    var image: String?
     
     init?(id: Int, name : String, address: String, route: Int, location: Location, sequence: Int, image: String) {
         // Initialize stored properties.
@@ -27,5 +27,26 @@ class CollectionPoint: Encodable, Decodable {
         self.location = location
         self.sequence = sequence
         self.image = image
+    }
+    
+    enum CodingKeys : String, CodingKey {
+        case id
+        case name
+        case address
+        case route
+        case sequence
+        case image
+        case location
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        address = try container.decode(String.self, forKey: .address)
+        route = try container.decode(Int.self, forKey: .route)
+        sequence = try container.decode(Int.self, forKey: .sequence)
+        location = try container.decode(Location.self, forKey: .location)
+        image = (try container.decodeIfPresent(String.self, forKey: .image)) ?? ""
     }
 }
