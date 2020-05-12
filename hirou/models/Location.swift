aa093fea1a63dca39d72c76c3f7d427850430ebe
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-12 18:03:15 +0530

    Fix Location struct Decodable behaviour
    Used singleValueContainer to handle string behaviour of the API

--- hirou/models/Location.swift
+++ hirou/models/Location.swift
@@ -11,4 +11,22 @@ import Foundation
 struct Location : Encodable, Decodable{
     var latitude: String = "";
     var longitude: String = "";
+    
+    init?(latitude: String, longitude: String) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+
+    enum CodingKeys : String, CodingKey {
+        case latitude
+        case longitude
+    }
+
+    init(from decoder: Decoder) throws{
+        let container = try decoder.singleValueContainer()
+        let locationString = try container.decode(String.self)
+        let locationCoordinates = locationString.split{$0 == ","}.map(String.init)
+        latitude = locationCoordinates[0]
+        longitude = locationCoordinates[1]
+    }
 }
