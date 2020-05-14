0b9d17ed17ff1d38b108ee0c6d78f91328927d42
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-14 12:38:37 +0530

    Fix route master list

--- hirou/CreateBaseRoute/RouteMasterViewController.swift
+++ hirou/CreateBaseRoute/RouteMasterViewController.swift
@@ -23,23 +23,7 @@ class RouteMasterViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        //         Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        //         self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-        //        navigationItem.leftBarButtonItem = editButtonItem
-        //
-        //        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        //        navigationItem.rightBarButtonItem = addButton
-        //        if let split = splitViewController {
-        //            let controllers = split.viewControllers
-        //            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? RouteDetailViewController
-        //        }
-        self.navigationItem.rightBarButtonItems = [self.editButtonItem, self.addRouteButton]
-        
+        self.navigationItem.rightBarButtonItems = [self.addRouteButton]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,67 +43,21 @@ class RouteMasterViewController: UITableViewController {
         super.viewWillAppear(animated)
     }
     
-    // MARK: - Table view data source
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.baseRoutes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! RouteTableViewCell
-        
         let route = baseRoutes[indexPath.row]
-        //        cell.textLabel!.text = route.name
         cell.routeNameLabel?.text = route.name
-        cell.customerLabel?.text = String(route.customer)
+        cell.customerLabel?.text = route.getCustomerName()
         cell.garbageTypeLabel?.text =  route.getGarbagesNameList()
         return cell
-    }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    @objc
-    func insertNewObject(_ sender: Any) {
-        
     }
     
     // MARK: - Segues
