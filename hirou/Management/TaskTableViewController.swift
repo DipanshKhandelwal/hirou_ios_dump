9e5ec505a7a0321782832a7f32a48d5c875da341
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-26 16:44:31 +0530

    Remove unused code from TaskTableViewController

--- hirou/Management/TaskTableViewController.swift
+++ hirou/Management/TaskTableViewController.swift
@@ -18,64 +18,11 @@ class TaskTableViewCell: UITableViewCell {
 
 class TaskTableViewController: UITableViewController {
     var taskRoutes = [TaskRoute]()
-    @IBOutlet weak var addRouteButton: UIBarButtonItem!
-    
-    private var datePicker:  UIDatePicker?
 
     var date: Date!
     
-    func generateRandomDate(daysBack: Int)-> Date?{
-        let day = arc4random_uniform(UInt32(daysBack))+1
-        let hour = arc4random_uniform(23)
-        let minute = arc4random_uniform(59)
-        
-        let today = Date(timeIntervalSinceNow: 0)
-        let gregorian  = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
-        var offsetComponents = DateComponents()
-        offsetComponents.day = -1 * Int(day - 1)
-        offsetComponents.hour = -1 * Int(hour)
-        offsetComponents.minute = -1 * Int(minute)
-        
-        let randomDate = gregorian?.date(byAdding: offsetComponents, to: today, options: .init(rawValue: 0) )
-        return randomDate
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let randomInt = Int.random(in: 0..<30)
-        date = generateRandomDate(daysBack: randomInt)
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        self.navigationItem.rightBarButtonItems = [self.editButtonItem, self.addRouteButton]
-        
-        let button =  UIButton(type: .system)
-        button.setTitle("Button", for: .normal)
-        button.addTarget(self, action: #selector(headerClicked), for: .touchUpInside)
-        navigationItem.titleView = button
-        
-        datePicker = UIDatePicker()
-        datePicker?.datePickerMode = .date
-        datePicker?.addTarget(self, action: #selector(TaskTableViewController.dateChanged(datePicker:)), for: .valueChanged)
-    }
-    
-    @objc
-    func headerClicked() {
-        print("Hello")
-    }
-    
-    @objc
-    func dateChanged(datePicker: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateFormat = "MM/dd/yyyy"
-        
-        print(dateFormatter.string(from: datePicker.date))
-        view.endEditing(true)
     }
 
     override func viewWillAppear(_ animated: Bool) {
