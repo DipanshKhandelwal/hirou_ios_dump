91fc0e0397a0d4f7f33d18d028ace40efc8d9186
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-29 13:52:05 +0530

    Add new TaskAddViewController

---
+++ hirou/Management/TaskAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskAddViewController.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 29/05/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class TaskAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
