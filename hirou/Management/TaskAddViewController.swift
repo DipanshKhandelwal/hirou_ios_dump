0290ef1fc0c37a633650c1ae5140ccb96b74ef43
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-29 14:46:50 +0530

    Add simple picker for selecting customer
    Data not inflated yet

--- hirou/Management/TaskAddViewController.swift
+++ hirou/Management/TaskAddViewController.swift
@@ -8,15 +8,72 @@
 
 import UIKit
 
-class TaskAddViewController: UIViewController {
+class TaskAddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var taskName: UITextField!
+    @IBOutlet weak var routeLabel: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let picker: UIPickerView
+        picker = UIPickerView()
+        picker.backgroundColor = UIColor.white
+
+        picker.delegate = self
+        picker.dataSource = self
+
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.donePicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(self.cancelPicker))
+
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+
+        routeLabel.inputView = picker
+        routeLabel.inputAccessoryView = toolBar
+        
         // Do any additional setup after loading the view.
     }
     
+    @objc
+    func cancelPicker() {
+        routeLabel.resignFirstResponder()
+    }
+    
+    @objc
+    func donePicker() {
+        print("done picker")
+    }
+    
 
+    @IBAction func cancelAddTask(_ sender: Any) {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    // picker
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row", row)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
