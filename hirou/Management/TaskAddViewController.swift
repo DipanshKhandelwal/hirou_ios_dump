4834f14fb1f7ab606dc808a479e1d76fdb0b6a35
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-29 15:08:12 +0530

    Fetch base routes and set selected route on click

--- hirou/Management/TaskAddViewController.swift
+++ hirou/Management/TaskAddViewController.swift
@@ -7,21 +7,49 @@
 //
 
 import UIKit
+import Alamofire
 
 class TaskAddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var taskName: UITextField!
     @IBOutlet weak var routeLabel: DisabledUITextField!
+    
+    var baseRoutes = [BaseRoute]()
+    var customerPicker = UIPickerView()
+    var selectedBaseRoute: BaseRoute?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCustomerPicker()
+    }
 
-        
-        let picker: UIPickerView
-        picker = UIPickerView()
-        picker.backgroundColor = UIColor.white
+    @IBAction func cancelAddTask(_ sender: Any) {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        AF.request("http://127.0.0.1:8000/api/base_route/", method: .get).response { response in
+            switch response.result {
+            case .success(let value):
+                let decoder = JSONDecoder()
+                self.baseRoutes = try! decoder.decode([BaseRoute].self, from: value!)
+                DispatchQueue.main.async {
+                    self.customerPicker.reloadAllComponents()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        super.viewWillAppear(animated)
+    }
 
-        picker.delegate = self
-        picker.dataSource = self
+    
+    // picker
+    
+    func setupCustomerPicker() {
+        customerPicker.backgroundColor = UIColor.white
+
+        customerPicker.delegate = self
+        customerPicker.dataSource = self
 
         let toolBar = UIToolbar()
         toolBar.barStyle = UIBarStyle.default
@@ -33,10 +61,8 @@ class TaskAddViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         toolBar.setItems([flexibleSpace, doneButton], animated: false)
         toolBar.isUserInteractionEnabled = true
 
-        routeLabel.inputView = picker
+        routeLabel.inputView = customerPicker
         routeLabel.inputAccessoryView = toolBar
-        
-        // Do any additional setup after loading the view.
     }
     
     @objc
@@ -44,34 +70,23 @@ class TaskAddViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         routeLabel.resignFirstResponder()
     }
     
-    @objc
-    func donePicker() {
-        print("done picker")
-    }
-    
-
-    @IBAction func cancelAddTask(_ sender: Any) {
-        _ = self.navigationController?.popViewController(animated: true)
-    }
-    
-    // picker
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 10
+        return baseRoutes.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return String(row)
+        return baseRoutes[row].name
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("row", row)
+        let route = self.baseRoutes[row]
+        self.selectedBaseRoute = route
+        self.routeLabel.text = route.name
     }
-    
     
     /*
     // MARK: - Navigation
