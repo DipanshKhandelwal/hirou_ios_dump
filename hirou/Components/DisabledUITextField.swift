2a0e73d98b157324bb768f49719d9591ab9a481a
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-05-29 14:45:45 +0530

    Add DisabledUITextField to restrict user to paste on the textField

---
+++ hirou/Components/DisabledUITextField.swift
@@ -0,0 +1,21 @@
+//
+//  DisabledUITextField.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 29/05/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class DisabledUITextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
