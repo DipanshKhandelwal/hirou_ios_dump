2cf94cf5db813337d6f0a064ed0daa36127c92a9
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-01 13:16:50 +0530

    Add TaskCollectionPoint annotation to TaskNavigationViewController map

--- hirou/Management/TaskNavigationViewController.swift
+++ hirou/Management/TaskNavigationViewController.swift
@@ -7,16 +7,121 @@
 //
 
 import UIKit
+import Mapbox
+import Alamofire
 
-class TaskNavigationViewController: UIViewController {
+class TaskNavigationViewController: UIViewController, MGLMapViewDelegate {
 
+    var id: String = ""
+    @IBOutlet var mapView: MGLMapView!
+    var selectedTaskCollectionPoint: TaskCollectionPoint!
+    var taskCollectionPoints = [TaskCollectionPoint]()
+    var annotations = [MGLPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.delegate = self
+        
+        self.id = UserDefaults.standard.string(forKey: "selectedTaskRoute")!
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        self.getPoints()
+    }
+    
+    func getPoints() {
+        let id = self.id
+        let url = "http://127.0.0.1:8000/api/task_route/"+String(id)+"/"
+        AF.request(url, method: .get).response { response in
+            //to get status code
+            switch response.result {
+            case .success(let value):
+                let route = try! JSONDecoder().decode(TaskRoute.self, from: value!)
+                let newCollectionPoints = route.taskCollectionPoints
+                self.taskCollectionPoints = newCollectionPoints.sorted() { $0.sequence < $1.sequence }
+                self.addPointsTopMap()
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func addPointsTopMap() {
+        self.mapView.removeAnnotations(self.annotations)
+        self.annotations = []
+        
+        for cp in self.taskCollectionPoints {
+            let annotation = MGLPointAnnotation()
+            let lat = Double(cp.location.latitude)!
+            let long = Double(cp.location.longitude)!
+            annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            annotation.title = cp.name
+            //            annotation.subtitle = "\(Double(annotation.coordinate.latitude)), \(Double(annotation.coordinate.longitude))"
+            annotations.append(annotation)
+        }
+        
+        mapView.addAnnotations(annotations)
+        
+        self.handleAutomaticZoom()
+    }
 
+    func handleAutomaticZoom() {
+        let annotations = self.annotations
+        
+        if annotations.count > 0 {
+            
+            let firstCoordinate = annotations[0].coordinate
+            
+            //Find the southwest and northeast point
+            var northEastLatitude = firstCoordinate.latitude
+            var northEastLongitude = firstCoordinate.longitude
+            var southWestLatitude = firstCoordinate.latitude
+            var southWestLongitude = firstCoordinate.longitude
+            
+            for annotation in annotations {
+                let coordinate = annotation.coordinate
+                
+                northEastLatitude = max(northEastLatitude, coordinate.latitude)
+                northEastLongitude = max(northEastLongitude, coordinate.longitude)
+                southWestLatitude = min(southWestLatitude, coordinate.latitude)
+                southWestLongitude = min(southWestLongitude, coordinate.longitude)
+                
+                
+            }
+            let verticalMarginInPixels = 250.0
+            let horizontalMarginInPixels = 250.0
+            
+            let verticalMarginPercentage = verticalMarginInPixels/Double(mapView.bounds.size.height)
+            let horizontalMarginPercentage = horizontalMarginInPixels/Double(mapView.bounds.size.width)
+            
+            let verticalMargin = (northEastLatitude-southWestLatitude)*verticalMarginPercentage
+            let horizontalMargin = (northEastLongitude-southWestLongitude)*horizontalMarginPercentage
+            
+            southWestLatitude -= verticalMargin
+            southWestLongitude -= horizontalMargin
+            
+            northEastLatitude += verticalMargin
+            northEastLongitude += horizontalMargin
+            
+            if (southWestLatitude < -85.0) {
+                southWestLatitude = -85.0
+            }
+            if (southWestLongitude < -180.0) {
+                southWestLongitude = -180.0
+            }
+            if (northEastLatitude > 85) {
+                northEastLatitude = 85.0
+            }
+            if (northEastLongitude > 180.0) {
+                northEastLongitude = 180.0
+            }
+            
+            mapView.setVisibleCoordinateBounds(MGLCoordinateBoundsMake(CLLocationCoordinate2DMake(southWestLatitude, southWestLongitude), CLLocationCoordinate2DMake(northEastLatitude, northEastLongitude)), animated: true)
+        }
+    }
     /*
     // MARK: - Navigation
 
