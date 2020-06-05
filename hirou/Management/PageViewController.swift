c9d2a0ddedd12848e574c0ee33691fef13b6c485
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-05 15:37:00 +0530

    Change date format on PageViewController

--- hirou/Management/PageViewController.swift
+++ hirou/Management/PageViewController.swift
@@ -64,7 +64,7 @@ class PageViewController: UIPageViewController {
         dataSource = self
         delegate = self
 
-        dateFormatter.dateFormat = "MM / dd / yyyy"
+        dateFormatter.dateFormat = "dd MMM yyyy"
         
         dateTitle.titleLabel?.sizeToFit()
         dateTitle.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
