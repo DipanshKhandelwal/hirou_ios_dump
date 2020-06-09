e2bfe5fe28e68d8a8fb013d9dc3510ce54cdb8ec
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-09 13:24:21 +0530

    Add environment struct to keep server url

---
+++ hirou/Environment.swift
@@ -0,0 +1,13 @@
+//
+//  Environment.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 09/06/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+
+struct Environment {
+    static let SERVER_URL = "http://127.0.0.1:8000/"
+}
