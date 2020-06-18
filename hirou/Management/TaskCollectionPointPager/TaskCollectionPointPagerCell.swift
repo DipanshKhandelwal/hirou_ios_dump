ac8769c4931efaf0fca7f06d61fcd68e96f201b7
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-18 17:58:25 +0530

    Create a custom TaskCollectionPointPager cell

---
+++ hirou/Management/TaskCollectionPointPager/TaskCollectionPointPagerCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCollectionPointPagerCell.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 16/06/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+import FSPagerView
+
+class TaskCollectionPointPagerCell: FSPagerViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
