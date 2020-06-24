0132d47e6b0ba08a91288a4ce4195fd3904d8b43
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-24 18:01:41 +0530

    Add task collection point notifications class

---
+++ hirou/Management/TaskCollectionPoint/TaskCollectionPointsNotifications.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionPointsNotifications.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 24/06/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var CollectionPointsHListUpdate: Notification.Name {
+        return .init(rawValue: "CollectionPoints.Update.HList")
+    }
+    
+    static var CollectionPointsVListUpdate: Notification.Name {
+        return .init(rawValue: "CollectionPoints.Update.VList")
+    }
+}
