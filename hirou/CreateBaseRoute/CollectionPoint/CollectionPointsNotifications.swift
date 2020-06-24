6e471cfe01fe9a1520da2167346a69f54f4edff5
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-06-24 19:10:53 +0530

    Add notifications for collection points

---
+++ hirou/CreateBaseRoute/CollectionPoint/CollectionPointsNotifications.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionPointsNotifications.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 24/06/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var CollectionPointsTableSelect: Notification.Name {
+        return .init(rawValue: "CollectionPoints.Select.Table")
+    }
+
+    static var CollectionPointsMapSelect: Notification.Name {
+        return .init(rawValue: "CollectionPoints.Select.Map")
+    }
+}
