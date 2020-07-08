a981f8354c5ca81b215e407eb07c9ebae4dd7929
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-07-09 02:47:21 +0530

    Add title to ReportAdminTableViewController

--- hirou/Management/ReportAdmin/ReportAdminTableViewController.swift
+++ hirou/Management/ReportAdmin/ReportAdminTableViewController.swift
@@ -22,6 +22,7 @@ class ReportAdminTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.title = "Report Admin"
     }
 
     // MARK: - Table view data source
