8338e6aeca7c28d18afbf7a85c313ccead627125
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-07-10 20:55:28 +0530

    Add ReportType modal

---
+++ hirou/models/ReportType.swift
@@ -0,0 +1,23 @@
+//
+//  ReportType.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 10/07/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+
+struct ReportType {
+    //MARK: Properties
+    var id: Int
+    var name: String
+    var description: String
+    
+    init?(id: Int, name : String, description: String) {
+        // Initialize stored properties.
+        self.id = id
+        self.name = name
+        self.description = description
+    }
+}
