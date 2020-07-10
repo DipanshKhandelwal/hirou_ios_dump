97a3e29682155afeb13ce619d27c420c22447119
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-07-10 19:58:04 +0530

    Make class TaskAmount

--- hirou/models/TaskAmount.swift
+++ hirou/models/TaskAmount.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct TaskAmount : Encodable, Decodable{
+    var id: Int
+    var route: Int
+    var garbage: Garbage
+    var amount: Int
+    var user: Int?
+    
+    init?(id: Int, route : Int, garbage: Garbage, amount: Int, user: Int) {
+        // Initialize stored properties.
+        self.id = id
+        self.route = route
+        self.garbage = garbage
+        self.amount = amount
+        self.user = user
+    }
+    
+    enum CodingKeys : String, CodingKey {
+        case id
+        case route
+        case garbage
+        case amount
+        case user
+    }
+}
