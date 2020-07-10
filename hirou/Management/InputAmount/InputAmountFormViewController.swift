403af4e4f48da14d00d64e2143aebc351841cb18
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-07-10 19:31:39 +0530

    Add InputAmountFormViewController

---
+++ hirou/Management/InputAmount/InputAmountFormViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InputAmountFormViewController.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 10/07/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class InputAmountFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Add Garbage Input"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
