51907afb40e45432fec99a2db7679c89bc1163da
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-07-15 16:21:52 +0530

    setup Pickers for collection point and report type

--- hirou/Management/ReportAdmin/ReportAdminFormViewController.swift
+++ hirou/Management/ReportAdmin/ReportAdminFormViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class ReportAdminFormViewController: UIViewController {
+class ReportAdminFormViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var collectionPointLabel: DisabledUITextField!
     @IBOutlet weak var reportTypeLabel: DisabledUITextField!
     
+    var collectionPointPicker = UIPickerView()
+    var reportTypePicker = UIPickerView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
+        
+        setupPickers()
     }
     
     @objc
@@ -28,6 +33,61 @@ class ReportAdminFormViewController: UIViewController {
     
     @IBAction func cancel(_ sender: Any) {
         _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    func setupPickers() {
+        setupCollectionPointPicker()
+        setupReportTypePicker();
+    }
+
+    func setupCollectionPointPicker() {
+        collectionPointPicker.backgroundColor = UIColor.white
+        
+        collectionPointPicker.delegate = self
+        collectionPointPicker.dataSource = self
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.collectionPointPickerDone))
+        
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        collectionPointLabel.inputView = collectionPointPicker
+        collectionPointLabel.inputAccessoryView = toolBar
+    }
+    
+    func setupReportTypePicker() {
+        reportTypePicker.backgroundColor = UIColor.white
+        
+        reportTypePicker.delegate = self
+        reportTypePicker.dataSource = self
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.reportTypePickerDone))
+        
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        reportTypeLabel.inputView = reportTypePicker
+        reportTypeLabel.inputAccessoryView = toolBar
+    }
+    
+    @objc
+    func collectionPointPickerDone() {
+        collectionPointLabel.resignFirstResponder();
+    }
+    
+    @objc
+    func reportTypePickerDone() {
+        reportTypeLabel.resignFirstResponder();
     }
 
     /*
