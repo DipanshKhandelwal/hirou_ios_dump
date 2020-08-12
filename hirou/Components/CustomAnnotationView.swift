a77ee63021c8a2d6dde5ea3d3994aa04fec64162
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-08-12 22:32:57 +0530

    Add custom annotation view

---
+++ hirou/Components/CustomAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomAnnotationView.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 12/08/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class CustomAnnotationView: MGLAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
