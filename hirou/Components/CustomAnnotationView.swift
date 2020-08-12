951253d384ec6dcd995c2cd125a40bdb8f7854a8
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-08-12 22:33:29 +0530

    Add default and selected styles for CustomAnnotationView

--- hirou/Components/CustomAnnotationView.swift
+++ hirou/Components/CustomAnnotationView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Mapbox
 
 class CustomAnnotationView: MGLAnnotationView {
 
@@ -16,6 +17,24 @@ class CustomAnnotationView: MGLAnnotationView {
     override func draw(_ rect: CGRect) {
         // Drawing code
     }
-    */
-
+     */
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Use CALayer’s corner radius to turn this view into a circle.
+        layer.cornerRadius = bounds.width / 2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Animate the border width in/out, creating an iris effect.
+        let animation = CABasicAnimation(keyPath: "borderWidth")
+        animation.duration = 0.1
+        layer.borderWidth = selected ? bounds.width / 4 : 1
+        layer.add(animation, forKey: "borderWidth")
+    }
 }
