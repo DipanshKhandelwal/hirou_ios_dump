7495bd2f47274ee3027323a58f5beb4e25ae1a98
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-11-12 17:29:26 +0530

    Create a new userDefaults constants file

---
+++ hirou/Constants/UserDefaultsConstants.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaultsConstants.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 12/11/20.
+//  Copyright © 2020 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
