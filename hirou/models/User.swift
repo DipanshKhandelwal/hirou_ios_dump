12e791965e9cdd25b66a0c6f5c3c508bce9f20d0
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2020-11-13 21:10:20 +0530

    Define User model

--- hirou/models/User.swift
+++ hirou/models/User.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct User : Encodable, Decodable{
+    var id: Int
+    var email: String?
+    var username: String?
+    var first_name: String?
+    var last_name: String?
+
+    init?(id: Int, email: String?, username: String?, first_name: String?, last_name: String?) {
+        self.id = id
+        self.email = email
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+    }
+    
+    enum CodingKeys : String, CodingKey {
+        case id
+        case email
+        case username
+        case first_name
+        case last_name
+    }
+}
