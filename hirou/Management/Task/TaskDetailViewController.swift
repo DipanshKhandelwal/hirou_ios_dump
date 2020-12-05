3187b6ed74d4b1770fb9077f70f784fe45d8d4f2
DipanshKhandelwal <deepanshrocks438@gmail.com>
2020-12-05 18:15:48 +0530

    Remove vehicle picker logic and API call

--- hirou/Management/Task/TaskDetailViewController.swift
+++ hirou/Management/Task/TaskDetailViewController.swift
@@ -9,63 +9,15 @@
 import UIKit
 import Alamofire
 
-class TaskDetailViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class TaskDetailViewController: UIViewController {
     @IBOutlet weak var customerLabel: UILabel!
-    @IBOutlet weak var vehicleLabel: UILabel!
     @IBOutlet weak var garbageLabel: UILabel!
-    @IBOutlet weak var vehiclePicker: UIPickerView!
-    
     
     @IBOutlet weak var deleteTaskButton: UIBarButtonItem!
-    
-    var vehiclesList = [Vehicle]()
-    var selectedVehicle: Vehicle!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        vehiclePicker.delegate = self
-        vehiclePicker.dataSource = self
-        vehiclePicker.isHidden = true
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(vehicleLabelPressed))
-        vehicleLabel.addGestureRecognizer(tap)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
-        self.view.addGestureRecognizer(tapGesture)
-        
         configureView()
-    }
-    
-    @objc
-    func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        vehiclePicker.isHidden = true
-    }
-    
-    @objc
-    func vehicleLabelPressed(sender: UITapGestureRecognizer) {
-        vehiclePicker.isHidden = !vehiclePicker.isHidden
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        let headers = APIHeaders.getHeaders()
-        AF.request(Environment.SERVER_URL + "api/vehicle/", method: .get, headers: headers).validate().responseJSON { response in
-            switch response.result {
-            case .success(let value):
-                print("response", value)
-                self.vehiclesList = []
-                for vehicle in value as! [Any] {
-                    let vehicleObj = Vehicle.getVehicleFromResponse(obj: vehicle as AnyObject)
-                    self.vehiclesList.append(vehicleObj)
-                }
-                self.vehiclePicker.reloadAllComponents()
-            case .failure(let error):
-                print(error)
-                //                completion(nil)
-            }
-            
-        }
     }
     
     var detailItem: Any? {
@@ -84,10 +36,6 @@ class TaskDetailViewController: UIViewController, UIPickerViewDelegate, UIPicker
             
             if let label = self.customerLabel {
                 label.text = task.customer?.name ?? "n/a"
-            }
-            
-            if let label = self.vehicleLabel {
-                label.text = "Vehicle"
             }
             
             if let label = self.garbageLabel {
@@ -144,34 +92,7 @@ class TaskDetailViewController: UIViewController, UIPickerViewDelegate, UIPicker
             }
         }
     }
-    
-    // picker
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.vehiclesList.count
-    }
-    
-    internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
-    {
-        if(self.vehiclesList.count >= row) {
-            return self.vehiclesList[row].registrationNumber
-        }
-        return ""
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if(self.vehiclesList.count > row) {
-            self.vehicleLabel.text = self.vehiclesList[row].registrationNumber
-            print(self.vehiclesList[row].registrationNumber)
-            //            self.selectedCustomerId = self.customers[row].id
-        }
-        self.vehiclePicker.isHidden = true
-    }
-    
+
     // MARK: - Navigation
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
