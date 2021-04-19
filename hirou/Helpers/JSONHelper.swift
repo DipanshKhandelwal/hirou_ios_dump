155c77424cc50d1aba7e4dbe272cf3da855b5738
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-04-19 23:44:53 +0530

    Create JSONHelper to convert JSON to dictionary

---
+++ hirou/Helpers/JSONHelper.swift
@@ -0,0 +1,20 @@
+//
+//  JSONHelper.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 18/04/21.
+//  Copyright © 2021 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+
+func convertToDictionary(text: String) -> [String: Any]? {
+    if let data = text.data(using: .utf8) {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    return nil
+}
