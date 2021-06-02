96ced164cb0f98c02c43d003358038aa0e3da5a6
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-06-03 04:23:48 +0530

    Add AppUtility helper for orientation lock

---
+++ hirou/Helpers/AppUtility.swift
@@ -0,0 +1,30 @@
+//
+//  AppUtility.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 03/06/21.
+//  Copyright © 2021 Dipansh Khandelwal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppUtility {
+
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+    
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+
+    /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+   
+        self.lockOrientation(orientation)
+    
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+        UINavigationController.attemptRotationToDeviceOrientation()
+    }
+
+}
