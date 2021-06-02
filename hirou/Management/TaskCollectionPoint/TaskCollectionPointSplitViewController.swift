71de1688e627611ed7b094ed04c3b5ad3d73126a
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-06-03 04:28:34 +0530

    Add TaskCollectionPointSplitViewController and add orientation lock

---
+++ hirou/Management/TaskCollectionPoint/TaskCollectionPointSplitViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCollectionPointSplitViewController.swift
+//  hirou
+//
+//  Created by Dipansh Khandelwal on 03/06/21.
+//  Copyright © 2021 Dipansh Khandelwal. All rights reserved.
+//
+
+import UIKit
+
+class TaskCollectionPointSplitViewController: UISplitViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        AppUtility.lockOrientation(.all)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        AppUtility.lockOrientation(.landscape, andRotateTo: .landscapeRight)
+    }
+}
