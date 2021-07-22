4cd7a166c6da3ca746cb236f4215dd9b89a6ff76
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2021-07-22 12:04:19 +0530

    Use filteredData property to show filtered elements

--- hirou/CreateBaseRoute/RouteMasterViewController.swift
+++ hirou/CreateBaseRoute/RouteMasterViewController.swift
@@ -19,6 +19,8 @@ class RouteMasterViewController: UIViewController, UITableViewDelegate, UITableV
     
     //    var detailViewController: RouteDetailViewController? = nil
     var baseRoutes = [BaseRoute]()
+    var filteredData = [BaseRoute]()
+    
     @IBOutlet weak var searchBar: UISearchBar! {
         didSet {
             searchBar.delegate = self
@@ -48,6 +50,7 @@ class RouteMasterViewController: UIViewController, UITableViewDelegate, UITableV
                 let decoder = JSONDecoder()
                 let baseRoutesList = try! decoder.decode([BaseRoute].self, from: value!)
                 self.baseRoutes = baseRoutesList.sorted() { $0.name < $1.name }
+                self.filteredData = self.baseRoutes
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -63,12 +66,12 @@ class RouteMasterViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.baseRoutes.count
+        return self.filteredData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! RouteTableViewCell
-        let route = baseRoutes[indexPath.row]
+        let route = filteredData[indexPath.row]
         cell.routeNameLabel?.text = route.name
         cell.customerLabel?.text = route.customer?.name ?? "n/a"
         cell.garbageTypeLabel?.text =  route.getGarbagesNameList()
@@ -80,7 +83,7 @@ class RouteMasterViewController: UIViewController, UITableViewDelegate, UITableV
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showRouteDetails" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let route = self.baseRoutes[indexPath.row]
+                let route = self.filteredData[indexPath.row]
                 let controller = (segue.destination as! RouteDetailViewController)
                 controller.detailItem = route as Any
             }
